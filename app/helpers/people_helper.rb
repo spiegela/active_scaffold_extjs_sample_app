1416bce89496e52df58bcbee9329e57a7f9fdf14
Aaron Spiegel <spiegela@Qin.local>
2008-03-19 22:58:02 -0500

    Started Nested Links for Associations

--- app/helpers/people_helper.rb
+++ app/helpers/people_helper.rb
@@ -1,2 +1,6 @@
 module PeopleHelper
+  def pets_column(record)
+    link_to record.pets.first(3).collect{|pet| h(pet.to_label)}.join(', '),
+      :url => {:action => 'nested', :associations => 'pets', :id => record.id}
+  end
 end
