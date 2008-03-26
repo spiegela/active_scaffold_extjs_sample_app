9f63f1ea6e7776f016e50c09e657ec6d5053cd3c
Aaron Spiegel <spiegela@Qin.local>
2008-03-26 07:10:15 -0500

    Started working on Pagination and Nested Support

--- app/controllers/people_controller.rb
+++ app/controllers/people_controller.rb
@@ -2,6 +2,7 @@ class PeopleController < ApplicationController
   active_scaffold :person do |config|
     config.columns  = [:name, :address, :city, :state, :pets]  
     config.list.columns  = [:name, :address, :city, :state, :pets]
+    config.list.per_page = 3
     config.create.columns = [:name, :address, :city, :state]
     config.frontend = 'extjs'
     config.actions.swap :search, :live_search
--- app/helpers/people_helper.rb
+++ app/helpers/people_helper.rb
@@ -1,6 +1,6 @@
 module PeopleHelper
   def pets_column(record)
-    link_to record.pets.first(3).collect{|pet| h(pet.to_label)}.join(', '),
-      :url => {:action => 'nested', :associations => 'pets', :id => record.id}
+    link_to_remote(record.pets.first(3).collect{|pet| h(pet.to_label)}.join(', '),
+      :url => {:action => 'nested', :associations => 'pets', :id => record.id} ).gsub(/'/, "\"")
   end
 end
